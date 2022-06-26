e3a44ed365fc989baadebb27e88ce2ed9299921a
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-06-26 16:34:04 -0400

    updated user and reaction schema

--- models/User.js
+++ models/User.js
@@ -2,7 +2,19 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema(
     {
-
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // regex to ensure an email is entered
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        }
     }
 )
 
