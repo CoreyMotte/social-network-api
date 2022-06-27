8fa66a4367c74ae3e79636677d74c3c1803db6e2
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-06-27 08:59:35 -0400

    added getallusers to usercontroller

---
+++ controllers/user-controller.js
@@ -0,0 +1,21 @@
+const { User } = require("../models");
+const { populate } = require("../models/User");
+
+const UserController = {
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+            path: "thoughts",
+            select: "-__v"
+        })
+        .select("-__v")
+        .sort({ _id: -1 })
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+    }
+}
+
+module.exports = UserController;
