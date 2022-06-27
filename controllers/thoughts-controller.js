1c6623792df3c88a7ffc7701ab4695dfacf19721
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-06-27 08:56:37 -0400

    added getallthoughts and getthoughtsbyid to thoughts controller

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -2,7 +2,38 @@ const { Thoughts, Users } = require('../models');
 
 const thoughtsController = {
 
-    
+    getAllThoughts(req, res) {
+        Thoughts.find({})
+            .populate({
+                path: "reactions",
+                select: "-__v",
+            })
+            .populate({
+                path: "thoughts",
+                select: "-__v"
+            })
+            .select("-__v")
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: "No thoughts found with this ID." });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 }
 
 module.exports = thoughtsController;
