105591f0f594f25af939eefe274559f8df8a785d
CoreyMotte <63756304+CoreyMotte@users.noreply.github.com>
2022-06-27 10:13:37 -0400

    added update and delete user routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,10 +4,10 @@ const { populate } = require("../models/User");
 const UserController = {
     getAllUsers(req, res) {
         User.find({})
-            .populate({
-                path: "Thoughts",
-                select: "-__v"
-            })
+            // .populate({
+            //     path: "Thoughts",
+            //     select: "-__v"
+            // })
             .select("-__v")
             .sort({ _id: -1 })
             .then((dbUserData) => res.json(dbUserData))
@@ -26,7 +26,7 @@ const UserController = {
             .select("-__v")
             .then((dbUserData) => {
                 if (!dbUserData) {
-                    res.status(404).json({ message: "No user found with this ID" });
+                    res.status(404).json({ message: "No users found with this ID!" });
                     return;
                 }
                 res.json(dbUserData);
@@ -41,7 +41,31 @@ const UserController = {
         User.create(body)
             .then((dbUserData) => res.json(dbUserData))
             .catch((err) => res.status(400).json(err));
-    }
+    },
+
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No users found with this ID!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => res.status(400).json(err));
+      },
+
+      deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No users found with this ID!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => res.status(400).json(err));
+      }
 }
 
 module.exports = UserController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,9 +3,13 @@ const router = require('express').Router();
 const {
     getAllUsers,
     getUserById,
-    createUser
+    createUser,
+    updateUser,
+    deleteUser
 } = require("../../controllers/user-controller")
 
 router.route("/").get(getAllUsers).post(createUser);
+
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
